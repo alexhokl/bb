81e6eb59bd06aca05475595c2fc7cf5d202124e0
alexhokl <alexhokl@gmail.com>
2017-01-22 14:44:14 +0800

    Added quiet mode to list command

--- command/list.go
+++ command/list.go
@@ -7,19 +7,29 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type listOptions struct {
+	isQuiet bool
+}
+
 // NewListCommand returns definition of command list
 func NewListCommand(cli *ManagerCli) *cobra.Command {
+	opts := listOptions{}
+
 	cmd := &cobra.Command{
 		Use:   "list",
 		Short: "List pull requests",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return runList(cli)
+			return runList(cli, opts)
 		},
 	}
+
+	flags := cmd.Flags()
+	flags.BoolVarP(&opts.isQuiet, "quiet", "q", false, "List IDs only")
+
 	return cmd
 }
 
-func runList(cli *ManagerCli) error {
+func runList(cli *ManagerCli, opts listOptions) error {
 	client := cli.Client()
 	cred := cli.UserCredential()
 	repo := cli.Repo()
@@ -38,11 +48,23 @@ func runList(cli *ManagerCli) error {
 		prInfo, _ := client.GetRequest(cred, repo, pr.ID)
 		isApproved := prInfo.IsApproved(cred.Username)
 		if isApproved {
-			color.Cyan(prInfo.ToShortDescription())
+			if opts.isQuiet {
+				color.Cyan(fmt.Sprintf("%d", prInfo.ID))
+			} else {
+				color.Cyan(prInfo.ToShortDescription())
+			}
 		} else if pr.Author.Username == cred.Username {
-			color.Blue(prInfo.ToShortDescription())
+			if opts.isQuiet {
+				color.Blue(fmt.Sprintf("%d", prInfo.ID))
+			} else {
+				color.Blue(prInfo.ToShortDescription())
+			}
 		} else {
-			color.Red(prInfo.ToShortDescription())
+			if opts.isQuiet {
+				color.Red(fmt.Sprintf("%d", prInfo.ID))
+			} else {
+				color.Red(prInfo.ToShortDescription())
+			}
 		}
 	}
 	return nil

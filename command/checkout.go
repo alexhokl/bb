e1d717b9c703256a38c6039c6bb4c741dbecb875
alexhokl <alexhokl@gmail.com>
2023-01-06 20:57:41 +0800

    Adapted command checkout to use access token

--- command/checkout.go
+++ command/checkout.go
@@ -1,61 +1,45 @@
 package command
 
 import (
+	"context"
 	"errors"
 	"fmt"
 
+	"github.com/alexhokl/bb/swagger"
+	"github.com/alexhokl/helper/authhelper"
 	"github.com/alexhokl/helper/git"
 	"github.com/spf13/cobra"
 )
 
 type checkoutOptions struct {
-	idOption
+	idOptions
 	isShowDifftool           bool
 	isShowStat               bool
 	isNoMergeFromDestination bool
 }
 
-// NewCheckoutCommand returns definition of command checkout
-func NewCheckoutCommand(cli *ManagerCli) *cobra.Command {
-	opts := checkoutOptions{}
+var checkoutOpts checkoutOptions
 
-	cmd := &cobra.Command{
-		Use:   "checkout",
-		Short: "Checkout the latest code of the branch of the specified pull request",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			if len(args) != 0 {
-				cli.ShowHelp(cmd, args)
-				return nil
-			}
-			errRepo := cli.SetRepository()
-			if errRepo != nil {
-				return errRepo
-			}
-			errCred := cli.SetCredentials()
-			if errCred != nil {
-				return errCred
-			}
-			return runCheckout(cli, opts)
-		},
-	}
-
-	flags := cmd.Flags()
-	flags.IntVarP(&opts.id, "id", "i", 0, "Pull request ID")
-	flags.BoolVarP(&opts.isShowDifftool, "difftool", "d", false, "Show difftool after checkout")
-	flags.BoolVarP(&opts.isShowStat, "stat", "s", false, "Show diff stats after checkout")
-	flags.BoolVar(&opts.isNoMergeFromDestination, "no-merge", false, "Do not merge from destination branch during checkout")
-
-	return cmd
+var checkoutCmd = &cobra.Command{
+	Use:   "checkout",
+	Short: "Checkout the latest code of the branch of the specified pull request",
+	RunE: runCheckout,
 }
 
-func runCheckout(cli *ManagerCli, opts checkoutOptions) error {
-	if opts.id <= 0 {
-		return errors.New("invalid pull request ID")
-	}
+func init() {
+	rootCmd.AddCommand(checkoutCmd)
 
-	client := cli.Client()
-	cred := cli.UserCredential()
-	repo := cli.Repo()
+	flags := checkoutCmd.Flags()
+	flags.Int32VarP(&checkoutOpts.id, "id", "i", 0, "Pull request ID")
+	flags.BoolVarP(&checkoutOpts.isShowDifftool, "difftool", "d", false, "Show difftool after checkout")
+	flags.BoolVarP(&checkoutOpts.isShowStat, "stat", "s", false, "Show diff stats after checkout")
+	flags.BoolVar(&checkoutOpts.isNoMergeFromDestination, "no-merge", false, "Do not merge from destination branch during checkout")
+}
+
+func runCheckout(_ *cobra.Command, _ []string) error {
+	if err := validateIDOptions(checkoutOpts.idOptions); err != nil {
+		return err
+	}
 
 	statusOutput, errStatus := git.GetStatus()
 	if errStatus != nil {
@@ -65,7 +49,25 @@ func runCheckout(cli *ManagerCli, opts checkoutOptions) error {
 		return errors.New("working directory is not prestine. Please stash your work and try again")
 	}
 
-	pr, err := client.GetRequest(cred, repo, opts.id)
+	savedToken, err := authhelper.LoadTokenFromViper()
+	if err != nil {
+		return err
+	}
+	auth := context.WithValue(context.Background(), swagger.ContextAccessToken, savedToken.AccessToken)
+	config := swagger.NewConfiguration()
+	client := swagger.NewAPIClient(config)
+
+	repo, err := getRepositoryInfoFromCurrentPath()
+	if err != nil {
+		return err
+	}
+
+	pr, _, err := client.PullrequestsApi.RepositoriesWorkspaceRepoSlugPullrequestsPullRequestIdGet(
+		auth,
+		checkoutOpts.id,
+		repo.Name,
+		repo.Org,
+	)
 	if err != nil {
 		return err
 	}
@@ -107,7 +109,7 @@ func runCheckout(cli *ManagerCli, opts checkoutOptions) error {
 	}
 	fmt.Printf("Pulled the latest code of branch %s.\n", pr.Source.Branch.Name)
 
-	if !opts.isNoMergeFromDestination {
+	if !checkoutOpts.isNoMergeFromDestination {
 		_, errMerge := git.Merge(pr.Destination.Branch.Name)
 		if errMerge != nil {
 			return errMerge
@@ -115,7 +117,7 @@ func runCheckout(cli *ManagerCli, opts checkoutOptions) error {
 		fmt.Printf("Merged from branch %s into %s.\n", pr.Destination.Branch.Name, pr.Source.Branch.Name)
 	}
 
-	if opts.isShowStat {
+	if checkoutOpts.isShowStat {
 		stat, errStat := git.DiffStat(pr.Destination.Branch.Name, "")
 		if errStat != nil {
 			return errStat
@@ -123,7 +125,7 @@ func runCheckout(cli *ManagerCli, opts checkoutOptions) error {
 		fmt.Println(stat)
 	}
 
-	if opts.isShowDifftool {
+	if checkoutOpts.isShowDifftool {
 		errDifftool := git.Difftool(pr.Destination.Branch.Name, "")
 		if errDifftool != nil {
 			return errDifftool
